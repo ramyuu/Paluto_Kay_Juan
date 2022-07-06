a1a6976877b48b6edaa43da0ad90bf3dfd2dc306
ramyuu <ramyuusama@gmail.com>
2022-07-06 21:24:41 +0800

    WORKING CRUD WITH PICTURES
    Working Crud with pictures
    ->Create, Read, Update, Delete (with pictures)
    ->Fix Gui Soon
    ->Add Other Pages Soon
    ->Add Login Soon
    -> Add Sorted View Pics Soon

---
+++ Create with Pic/Controllers/MenuController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Paluto_Kay_Juan.Data;
+using Paluto_Kay_Juan.Models;
+
+namespace Paluto_Kay_Juan.Controllers
+{
+    public class MenuController : Controller
+    {
+        private readonly IWebHostEnvironment webHostEnvironment;
+
+        private readonly AppDbContext _context;
+
+        public MenuController(AppDbContext context, IWebHostEnvironment webHost)
+        {
+            _context = context;
+            webHostEnvironment = webHost;
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            AdminModel adminModel = new AdminModel();
+            return View(adminModel);
+        }
+
+        [HttpPost]
+        public IActionResult Create(AdminModel adminModel)
+        {
+            string uniqueFileName = UploadFile(adminModel);
+            adminModel.FoodImgUrl = uniqueFileName;
+
+            _context.Attach(adminModel);
+            _context.Entry(adminModel).State = EntityState.Added;
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+
+
+        public string UploadFile(AdminModel adminModel)
+        {
+            string uniqueFileName = null;
+            if (adminModel.UploadedImage != null)
+            {
+                string path = Path.Combine(webHostEnvironment.WebRootPath, "images");
+                uniqueFileName = Guid.NewGuid().ToString() + "-" + adminModel.UploadedImage.FileName;
+                string filePath = Path.Combine(path, uniqueFileName);
+
+                using(var fs = new FileStream(filePath, FileMode.Create))
+                {
+                    adminModel.UploadedImage.CopyTo(fs);
+                }
+            }
+            return uniqueFileName;
+        }
+
+        public IActionResult Index()
+        {
+            List<AdminModel> adminModels;
+            adminModels = _context.Menus.ToList();
+            return View(adminModels);
+        }
+    }
+}
---
+++ Create with Pic/Migrations/20220706061745_addImageUrl.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Paluto_Kay_Juan.Migrations
+{
+    public partial class addImageUrl : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FoodImgUrl",
+                table: "Menus",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FoodImgUrl",
+                table: "Menus");
+        }
+    }
+}
